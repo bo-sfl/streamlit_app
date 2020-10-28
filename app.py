d6c02a4de3fc0932c74d65bb0dcc336a53ad5478
bo-sfl <bli@sflscientific.com>
2020-10-27 17:05:32 -0700

    update

--- app.py
+++ app.py
@@ -4,21 +4,14 @@ from plotly import graph_objs as go
 from pathlib import Path
 import base64
 import pandas as pd
+import random
+import datetime
 
 
 def run_inference(patient_data):
     time.sleep(3)
-    predictions = {
-        "Pathway A":60,
-        "Pathway B":55,
-        "Pathway C":51,
-        "Pathway D":70,
-    }
-    if patient_data["pathways"]:
-        return {k:predictions[k] for k in patient_data["pathways"]}
-    else:
-        return predictions
-
+    _base = patient_data["meta"][-1]
+    return {k:_base + random.randint(1, 20)for k in patient_data["pathways"]}
 
 def plot_predictions(patient_data, preditions):
     x_size = len(patient_data["meta"])
@@ -39,7 +32,7 @@ def plot_predictions(patient_data, preditions):
         for k in sorted(preditions.keys())
     ])
     fig.layout.xaxis.title= "Date"
-    fig.layout.yaxis.title= "Prediction"
+    fig.layout.yaxis.title= "Pharmacy Margin Prediction"
     fig.update_layout(
         shapes=[
             dict(
@@ -51,6 +44,17 @@ def plot_predictions(patient_data, preditions):
                 xref= 'x', x0= x_size-1, x1= x_size-1
             ),
     ])
+    today = datetime.datetime.today()
+    current_month = today.strftime("%b %Y")
+    date_3m_later = (today+datetime.timedelta(days=90)).strftime("%b %Y")
+    fig.update_layout(
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals = [4,5],
+        ticktext = [current_month, date_3m_later],
+        range = [0,5.3], 
+    )
+)
     return fig
 
 
@@ -58,6 +62,35 @@ def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
     return encoded
+
+@st.cache(show_spinner=False)
+def load_local_data():
+    """
+    Loading static meta data
+    Using dummy data now 
+    """
+    data = {}
+    # Load facilities
+    with open("data/facilities.txt", "r") as f:
+        facilities = [line.strip() for line in f.readlines()]
+
+    # Load disease and pathways
+    meta = pd.read_csv("data/diseases_and_pathways.csv")
+    _mapping = {
+        disease : _df["pathway"].values
+        for disease, _df in meta.groupby("DiseaseType")
+    }
+    diseases = meta.DiseaseType.nunique()
+
+    data["meta"] = {
+        facility:{
+            disease:_mapping[disease] 
+            for disease in random.sample(list(_mapping),k=4)
+        }
+        for facility in facilities
+    }
+
+    return data
 
 
 @st.cache(show_spinner=False)
@@ -74,24 +107,18 @@ def get_data(patient_mrn):
     return [10,20,30,40,50]
 
 @st.cache(show_spinner=False)
-def get_facility_list(data):
-    return [
-        "PA-ERMC",
-        "GA-SERMC",
-        "AZ-WRMC",
-        "IL-MRMC",
-        "OK-SRMC",
-            ]
+def get_facility_list(meta_data, patient_data):
+    return sorted(list(meta_data['meta'].keys()))
 
 
 @st.cache(show_spinner=False)
-def get_disease_list(data):
-    return ["A", "B", "C"]
+def get_disease_list(meta_data, patient_data):
+    return sorted(list(meta_data['meta'][patient_data["facility"]].keys()))
 
 
 @st.cache(show_spinner=False)
-def get_pathway_list(data):
-    return ["Pathway A", "Pathway B", "Pathway C", "Pathway D"]
+def get_pathway_list(meta_data, patient_data):
+    return sorted(meta_data['meta'][patient_data["facility"]][patient_data["disease"]])
 
 
 def main():
@@ -116,7 +143,8 @@ def main():
         """<style> .block-container {padding-top: 1rem;}
         </style>
         """, unsafe_allow_html=True) # This doesn't work
-
+    ## Loading
+    meta_data = load_local_data()
     ## Main App
     st.sidebar.header('Inputs')
     patient_mrn = st.sidebar.text_input("Please enter the Patient MRN:")
@@ -132,15 +160,15 @@ def main():
     if patient_data["meta"]:
         patient_data["facility"] = st.sidebar.selectbox(
             "Please specific facility:", 
-            get_facility_list(patient_data),
+            get_facility_list(meta_data, patient_data),
             key=f"{patient_mrn}_facility",
             )
         patient_data["disease"] = st.sidebar.selectbox(
             "Please specific disease type:",
-            get_disease_list(patient_data),
+            get_disease_list(meta_data, patient_data),
             key=f"{patient_mrn}_disease"
             )
-        available_pathway = get_pathway_list(patient_data)
+        available_pathway = get_pathway_list(meta_data, patient_data)
         pathways_bool = [st.sidebar.checkbox(_pathway,True,key=str(patient_mrn)+_pathway) for _pathway in available_pathway]
         patient_data["pathways"] = [_x for _x, _y in zip(available_pathway, pathways_bool) if _y]
         run_prediction = st.sidebar.button('Run')
@@ -161,8 +189,9 @@ def main():
     # with pred_container.beta_container():
     st.plotly_chart(plot_predictions(patient_data, predictions),use_container_width=True, config={"displaylogo":False} )
     df = pd.DataFrame.from_dict(predictions, orient='index', columns=['Pharmacy Margin Prediction'])
+    df.index.name = "Pathway"
     df['Pharmacy Margin Prediction'] = df['Pharmacy Margin Prediction'].round().apply(int)
-    st.dataframe(df.style.highlight_max(axis=0),width=900)
+    st.dataframe(df.style.highlight_max(axis=0), width=900)
 
     with st.beta_expander("Learn about the methodology:"):
         st.write("""
